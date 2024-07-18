64fba8dfe98c507f98dfbca373a3950ce99a660c
于邦飞 <528154320@qq.com>
2024-07-18 15:44:01 +0800

    add prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,8 @@
-export default {
-	
-}
+const styleguide = require('@vercel/style-guide/prettier');
+
+module.exports = {
+  ...styleguide,
+  plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+  noVar: 'error',
+  endOfLine: 'auto',
+};
