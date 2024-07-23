7e53bcab751ee9d0a3377e35f64053b0b7ebfdb3
于邦飞 <528154320@qq.com>
2024-07-23 10:59:22 +0800

    modify avatar style

--- src/app/_components/Avatar.tsx
+++ src/app/_components/Avatar.tsx
@@ -6,7 +6,6 @@ import {
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
 } from '@/components/ui/navigation-menu';
@@ -14,6 +13,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { handleSignOut } from '@/actions';
 import { toast } from 'sonner';
+import { Button } from '@/components/ui/button';
 
 export default function NavigationMenuDemo({
   children,
@@ -35,18 +35,18 @@ export default function NavigationMenuDemo({
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>{children}</NavigationMenuTrigger>
+          <NavigationMenuTrigger className="bg-transparent">
+            {children}
+          </NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul className="p-6 md:w-[200px] lg:w-[200px]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <div>
-                    <Link href="/setting">setting</Link>
-                    <div onClick={signOut}>signOut</div>
-                  </div>
-                </NavigationMenuLink>
-              </li>
-            </ul>
+            <div className="flex w-48 flex-col gap-y-1 p-2">
+              <Button asChild variant="ghost" className="h-8">
+                <Link href="/setting">setting</Link>
+              </Button>
+              <Button onClick={signOut} variant="ghost" className="h-8">
+                signOut
+              </Button>
+            </div>
           </NavigationMenuContent>
         </NavigationMenuItem>
       </NavigationMenuList>
